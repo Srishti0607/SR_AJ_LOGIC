1d69f4b23a1cf463fc4dddb25f58c40e1ab621e3
srishti.rathi <srishti.rathi@ril.com>
2023-03-21 12:32:05 +0530

    Btns in multi-level accordian

--- src/app/multi-level-accordian/multi-level-accordian.component.ts
+++ src/app/multi-level-accordian/multi-level-accordian.component.ts
@@ -10,6 +10,8 @@ import { Subscription } from "rxjs";
 export class MultiLevelAccordianComponent implements OnInit {
   public subscriptionsList: Subscription[] = []; // to unsubscribe API calls
   employeeDetails: any = [];
+  orderFlag:boolean = false;
+  orderDetFlag:boolean = false;
 
   constructor(private landingSrv: LandingService) { }
 
@@ -17,10 +19,10 @@ export class MultiLevelAccordianComponent implements OnInit {
     this.getEmployeesDetails();
   }
 
-  getEmployeesDetails(){
+  getEmployeesDetails() {
     this.subscriptionsList.push(
       this.landingSrv.getEmployeeData().subscribe((data: any) => {
-        if (data) {         
+        if (data) {
           this.employeeDetails = data;
         }
       })
@@ -28,36 +30,98 @@ export class MultiLevelAccordianComponent implements OnInit {
 
   }
 
-  appendTd(event,index,subIndex,opr){
+  appendTd(event, index, subIndex, opr) {
     let target = event.currentTarget,
-    classList = target.classList;
-    if(opr == 'E'){
-    if (this.employeeDetails[index].isActive) {
-      this.employeeDetails[index]['order'].forEach(order => { 
-        if(order['isActive'] != undefined){
-          delete order['isActive']; 
-        } 
-      });
-      this.employeeDetails[index].isActive = false;
-      classList.remove('fa-minus');
-        classList.add('fa-plus');
-    } else {
-      this.employeeDetails[index].isActive = true;
-      classList.remove('fa-plus');
-      classList.add('fa-minus');
-    }
-  }else{
-    if (this.employeeDetails[index]['order'][subIndex].isActive) {
-      this.employeeDetails[index]['order'][subIndex].isActive = false;     
+      classList = target.classList;
+    if (opr == 'E') {
+      if (this.employeeDetails[index].isActive) {
+        this.employeeDetails[index]['order'].forEach(order => {
+          if (order['isActive'] != undefined) {
+            delete order['isActive'];
+          }
+        });
+        this.employeeDetails[index].isActive = false;
         classList.remove('fa-minus');
         classList.add('fa-plus');
+      } else {
+        this.employeeDetails[index].isActive = true;
+        classList.remove('fa-plus');
+        classList.add('fa-minus');
+      }
     } else {
-      this.employeeDetails[index]['order'][subIndex].isActive = true;
-      classList.remove('fa-plus');
-      classList.add('fa-minus');
+      if (this.employeeDetails[index]['order'][subIndex].isActive) {
+        this.employeeDetails[index]['order'][subIndex].isActive = false;
+        classList.remove('fa-minus');
+        classList.add('fa-plus');
+      } else {
+        this.employeeDetails[index]['order'][subIndex].isActive = true;
+        classList.remove('fa-plus');
+        classList.add('fa-minus');
+      }
+
+    }
+  }
+
+  toggleAll(opr) {
+    if (opr == 1) {
+      this.employeeDetails.forEach(data => {
+        if (data['order'].length != 0) {
+          let target: HTMLElement;
+          let classList: any
+          target = document.getElementById('Id-' + data['EmployeeID'])
+          classList = target.classList;
+          data['isActive'] = true;
+          classList.add('fa-minus');
+          classList.remove('fa-plus');
+        }
+      })
+    } else if (opr == 3) {
+      this.employeeDetails.forEach(data => {
+        if (data['order'].length != 0) {
+          let target: HTMLElement;
+          let classList: any
+          target = document.getElementById('Id-' + data['EmployeeID'])
+          classList = target.classList;
+          data['isActive'] = false;
+          classList.remove('fa-minus');
+          classList.add('fa-plus');
+        }
+      })
+    } else if (opr == 2) {
+      this.employeeDetails.forEach(data => {
+        if (data['order'].length != 0) {
+          data['order'].forEach(orderDet => {
+            if(orderDet['orderDetails'].length != 0){
+            orderDet['isActive'] = true;
+            let target: HTMLElement;
+            let classList: any
+            target = document.getElementById('Id-' + orderDet['orderid'])
+            classList = target.classList;
+            classList.add('fa-minus');
+            classList.remove('fa-plus');
+            }
+          });
+        }
+      })
+    } else{
+      this.employeeDetails.forEach(data => {
+        if (data['order'].length != 0) {
+          data['order'].forEach(orderDet => {
+            if(orderDet['orderDetails'].length != 0){
+            orderDet['isActive'] = false;
+            let target: HTMLElement;
+            let classList: any
+            target = document.getElementById('Id-' + orderDet['orderid'])
+            classList = target.classList;
+            classList.remove('fa-minus');
+            classList.add('fa-plus');
+            }
+          });
+        }
+      })
+
     }
 
-  }
   }
 
 }
