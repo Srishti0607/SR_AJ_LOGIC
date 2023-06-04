4d973657f7295be011dbec55350df962bd840dbb
srishti0607 <srishti2906@gmail.com>
2023-06-05 00:29:31 +0530

    Correction for custom structural directive

--- src/app/structural-directive/structural-directive.component.ts
+++ src/app/structural-directive/structural-directive.component.ts
@@ -10,11 +10,53 @@ import { Subscription } from 'rxjs';
 export class StructuralDirectiveComponent implements OnInit {
   condition: boolean = true;
   public subscriptionsList: Subscription[] = []; // to unsubscribe API calls
-  obj = {
-    a: 'one',
-    b: 'two',
-    c: 'three'
-  }
+  obj = [
+    {
+        "EmployeeID": 1,
+        "lastname": "Davolio",
+        "firstname": "Nancy"
+    },
+    {
+        "EmployeeID": 2,
+        "lastname": "Fuller",
+        "firstname": "Andrew"
+    },
+    {
+        "EmployeeID": 3,
+        "lastname": "Leverling",
+        "firstname": "Janet"
+    },
+    {
+        "EmployeeID": 4,
+        "lastname": "Peacock",
+        "firstname": "Margaret"
+    },
+    {
+        "EmployeeID": 5,
+        "lastname": "Buchanan",
+        "firstname": "Steven"
+    },
+    {
+        "EmployeeID": 6,
+        "lastname": "Suyama",
+        "firstname": "Michael"
+    },
+    {
+        "EmployeeID": 7,
+        "lastname": "King",
+        "firstname": "Robert"
+    },
+    {
+        "EmployeeID": 8,
+        "lastname": "Callahan",
+        "firstname": "Laura"
+    },
+    {
+        "EmployeeID": 9,
+        "lastname": "Dodsworth",
+        "firstname": "Anne"
+    }
+]
   name = 'Kameshwara';
   
 
