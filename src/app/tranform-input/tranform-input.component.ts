13554c2660deb4b057a4796be23b26564cd15ae7
srishti0607 <srishti2906@gmail.com>
2023-07-02 21:06:11 +0530

    Input transformation

--- src/app/tranform-input/tranform-input.component.ts
+++ src/app/tranform-input/tranform-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-tranform-input',
@@ -6,15 +6,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./tranform-input.component.css']
 })
 export class TranformInputComponent {
-
 }
 
 @Component({
   selector: 'app-tranform-input-child',
-  template: `<div></div>`
+  template: `<div><span>Value changed after transformation for boolean - {{sampleInputParameter}}<br/></span>
+  <span>Value changed after transformation for number - {{sampleNumberParameter}}</span><br/>
+  <span>Value changed after transformation for number - {{numberParameterNotHandled}}</span><br/>
+  <span>Value changed after transformation for number - {{conversionParam}}</span><br/>
+  <span>Will it throw error {{title}}</span></div>`
   // styleUrls: ['']
 })
 export class TranformInputChildComponent {
-  // @Input({ transform: booleanAttribute }) sampleInputParameter = false;
+  @Input({ transform: booleanAttribute }) sampleInputParameter;
+  @Input({transform : numberAttribute}) numberParameterNotHandled ;
+  @Input({ transform: (value: unknown) => numberAttribute(value, 42) }) sampleNumberParameter;
+  @Input({transform: booleanAttribute}) conversionParam ;
+  @Input({ required: false }) title: string = '';
 
 }
